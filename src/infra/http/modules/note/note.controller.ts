d2fe022cd646c1eb2552dac5ff16388a8246aa1b
Daiana Michels <daianaem.12@gmail.com>
2024-06-01 09:41:19 -0300

    feat: add endpoint put for edit one note

--- src/infra/http/modules/note/note.controller.ts
+++ src/infra/http/modules/note/note.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Post, Request } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put, Request } from '@nestjs/common';
 import { CreateNoteUseCase } from 'src/modules/note/useCases/createNoteUseCase/createNoteUseCase';
 import { AuthRequestModel } from '../auth/models/authRequestModel';
 import { NoteViewModel } from '../user/viewModels/noteViewModel';
 import { CreateNote } from './dtos/createNote.dto';
+import { EditNote } from './dtos/editNote';
+import { EditNoteUseCase } from 'src/modules/note/useCases/editNoteUseCase/editNoteUseCase';
 
 @Controller('notes')
 export class NoteController {
-  constructor(private createNoteUseCase: CreateNoteUseCase) {}
+  constructor(
+    private createNoteUseCase: CreateNoteUseCase,
+    private editNoteUseCase: EditNoteUseCase,
+  ) {}
 
   @Post()
   async createNote(@Request() request: AuthRequestModel, @Body() body: CreateNote) {
@@ -19,5 +24,17 @@ export class NoteController {
     });
 
     return NoteViewModel.toHtpp(user);
+  }
+
+  @Put('/:id')
+  async updateNote(@Request() request: AuthRequestModel, @Param('id') noteId: string, @Body() body: EditNote) {
+    const { title, description } = body;
+
+    await this.editNoteUseCase.execute({
+      noteId,
+      userId: request.user.id,
+      title,
+      description,
+    });
   }
 }
